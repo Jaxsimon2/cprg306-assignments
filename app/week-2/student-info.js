7a9006f65001190dd0b3fb3753fa150984e7f0c7
Jaxsimon <153473979+Jaxsimon2@users.noreply.github.com>
2024-09-11 13:03:24 -0600

    Week 2 Assignment
    Added Week-2 folder and page, StudentInfo component and linked the week-2 page with the home page.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <p>Jaxson Burdett</p>
+            <Link href="https://github.com/Jaxsimon2?tab=repositories">GitHub</Link>
+        </main>
+    );
+}
