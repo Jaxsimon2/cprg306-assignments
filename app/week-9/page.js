762b2a5eaf0578b6014dabd72df336725c6b81b1
Jaxsimon <153473979+Jaxsimon2@users.noreply.github.com>
2024-11-06 13:56:36 -0700

    Week 9 Assignment

---
+++ app/week-9/page.js
@@ -0,0 +1,33 @@
+"use client";
+
+import { useUserAuth } from "./_utils/auth-context";
+
+export default function Page(){
+const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+const login = async () => {
+    await gitHubSignIn();
+}
+
+const logout = async () => {
+    await firebaseSignOut();
+}
+
+    return(
+        <main>
+            <h1>Week 9 Assignment</h1>
+
+            <div>
+                {user ? (
+                    <div>
+                        <p> Welcome, {user.displayName}!</p>
+                        <button onClick={logout}>Logout</button>
+                    </div>
+                ): (
+                    <button onClick={login}>Login with GitHub</button>
+                )}
+                
+            </div>
+        </main>
+    );
+}
